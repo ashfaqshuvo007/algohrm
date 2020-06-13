2bb3fe1884aa094453b6dcbf2180f6e26bf0d082
Ashfaq H Ahmed <ashfaqshuvo14@gmail.com>
2020-06-13 23:23:41 +0600

    Made necessary Changes. Basic operations done.

---
+++ resources/views/administrator/layouts/footer.blade.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+        <b>{{ __('Version') }}</b> 1.0.4
+    </div>
+    <strong>{{ __('Copyright') }} &copy; {{ date('Y')}} <a href="https://algodevs.com" target="_blank">{{ __('Algo HRM') }}</a>.</strong> {{ __('All rights reserved.') }}
+</footer>
